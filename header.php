84cbefa9f433cd3016eda81f06b4fc7e3d6d103e
Jonathan-92 <jonte790@gmail.com>
2016-06-13 14:08:55 +0200

    include a php header file instead of repetedly defining the identical header section on each page

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+echo '<div id="header">
+	<table class="header-images">
+		<tr>
+			<img src="images large/DSC02753.jpg">
+			<img class="overflow" src="images large/DSC02757.jpg">
+			<img class="overflow" src="images large/DSC02758.jpg">
+		</tr>
+	</table>
+</div>';
+?>
