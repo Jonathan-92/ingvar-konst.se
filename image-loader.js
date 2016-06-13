05f65e751320afe9c7a125088b9456b4d51f721c
Jonathan-92 <jonte790@gmail.com>
2016-06-13 14:15:09 +0200

    clean up unnecessary javascript

--- image-loader.js
+++ image-loader.js
@@ -1,4 +1,3 @@
-// $('#gallery').load(callForImages());
 callForImages();
 
 function callForImages() {
@@ -24,12 +23,10 @@ function callForImages() {
 
 function loadImages(images) {
 	var imageContainer = document.getElementById("gallery");
-	console.log(images);
 	for (var image in images) {
 		if (!images.hasOwnProperty(image)) {
 			alert("!");
 		}
-		console.log(image);
 		var newLink = document.createElement("a");
 		newLink.setAttribute("class", "image");
 		newLink.setAttribute("href", image);
@@ -38,13 +35,6 @@ function loadImages(images) {
 		var newImage = document.createElement("img");
 		newImage.setAttribute("src", image);
 		newImage.setAttribute("class", "thumb");
-		newImage.width = images[image]["width"];
-		// newImage.height = images[image]["height"];
-		// newImage.width = 365;
-		var w = newImage.width *= 0.5;
-		// var w = newImage.getAttribute("width");
-		// var newWidth = w * 0.5;
-		// newImage.setAttribute("width", newWidth);
 		newLink.appendChild(newImage);
 	}
 }
